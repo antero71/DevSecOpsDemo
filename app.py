8d2955fc256fe876c03dd2fd9002812671c571cf
Antero Oikkonen <antero.oikkonen@gmail.com>
2025-04-04 12:59:58 +0300

    refactor(flask): use echo.html with render_template instead of render_template_string

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ def home():
 @app.route("/echo", methods=["GET", "POST"])
 def echo():
     data = request.args.get("input") or request.form.get("input")
-    return render_template_string("You sent: {{ data }}", data=data)
+    return render_template("echo.html", data=data)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
